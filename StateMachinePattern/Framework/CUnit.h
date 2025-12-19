139fda6e26966e5ec417eb1e5dfae245b3095460
bongsoo.kim <bongsoo.kim@techwing.co.kr>
2025-12-19 15:47:38 +0900

    ㄹㅇㄻㅇㄴㅁㄹㅇㄴㅁㄹ

--- StateMachinePattern/Framework/CUnit.h
+++ StateMachinePattern/Framework/CUnit.h
@@ -10,13 +10,11 @@ public:
 	// StartSwitch를 레퍼런스로 받도록 변경
     CUnit(std::string name, IOPSwitch& startSwitch);
     virtual ~CUnit();
-
     void setStep(IStep* pStep);
-    bool sequence() override; // override 키워드 추가
 
 protected:
+    bool sequence() override; // override 키워드 추가
     std::string m_strName;
-    IStep* m_pCurrentStep;
-
-    IOPSwitch& m_StartSwitch; // 포인터에서 레퍼런스로 변경
+    IStep*      m_pCurrentStep;
+    IOPSwitch&  m_StartSwitch; // 포인터에서 레퍼런스로 변경
 };
